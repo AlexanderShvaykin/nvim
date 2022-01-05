4b03eab98a047f1e2b6f15c7f63c26ba231d3aaa
Alexander Shvaykin <skiline.alex@gmail.com>
2022-01-05 12:19:54 +0300

    Initial Setup

---
+++ plugins/lspconfig.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.setup_lsp = function(attach, capabilities)
+   local lspconfig = require "lspconfig"
+   
+   local servers = { "html", "cssls" }
+
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = attach,
+         capabilities = capabilities,
+         flags = {
+            debounce_text_changes = 150,
+         },
+      }
+   end
+
+   lspconfig.solargraph.setup {
+     on_attach = attach,
+     capabilities = capabilities,
+     useBundler = false,
+   }
+end
+
+return M
