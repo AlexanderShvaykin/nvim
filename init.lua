dd92d015bb0c8e75f428273fc7050c58c3f50af9
Alexander Shvaykin <skiline.alex@gmail.com>
2022-01-06 15:41:26 +0300

    Adds splitJoin

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ hooks.add("install_plugins", function(use)
   use("nathom/filetype.nvim")
   use("tpope/vim-rails")
   use('dyng/ctrlsf.vim')
+  use("AndrewRadev/splitjoin.vim")
 end)
 
 -- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
