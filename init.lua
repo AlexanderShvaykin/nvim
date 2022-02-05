d642ef4fae034473327a7ec0326c0bce0d0b47b6
Alexander Shvaykin <skiline.alex@gmail.com>
2022-02-05 23:54:38 +0300

    Fixes maps

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("custom.autocmds")
 -- MAPPINGS
 local map = require("core.utils").map
 
-map("n", "<leader>cc", ":Telescope <CR>", opt)
-map("n", "<leader>gf", ":Telescope git_files <CR>", opt)
+map("n", "<leader>cc", ":Telescope <CR>")
+map("n", "<leader>gf", ":Telescope git_files <CR>")
+map("n", "ff", ":CtrlSF ")
 
