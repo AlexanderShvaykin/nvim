3bcd86fa7b73173996ad67819d38ef6e01bf1e6c
Alexander Shvaykin <skiline.alex@gmail.com>
2022-11-11 21:49:33 +0400

    Adds cmp sourses

--- plugins/init.lua
+++ plugins/init.lua
@@ -53,8 +53,12 @@ return {
           ["<C-d>"] = cmp.mapping.scroll_docs(-8),
         },
         sources = {
+          { name = "luasnip" },
+          { name = "nvim_lsp" },
+          { name = "nvim_lua" },
+          { name = "path" },
           { name = 'cmp_tabnine' },
-        }
+        },
       }
     end,
   },
